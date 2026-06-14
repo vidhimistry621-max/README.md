e025e2826eeb3375c6ba9ef97a29c0f5a1cf2bac
vidhimistry621-max <vidhimistry621@gmail.com>
2026-06-14 20:30:01 +0530

    Add files via upload
    Day 12 - Random Quote Generator Using Fetch API Completed

--- script.js
+++ script.js
@@ -1,157 +1,63 @@
-let movies = [];
+const quote = document.querySelector(".quote");
+const author = document.querySelector(".author");
+const quotebtn = document.querySelector(".quote-btn");
+const favbtn = document.querySelector(".favbtn");
+const counter = document.querySelector(".counter");
 
+let quoteCount = 0;
 
-const addMovie = () => {
+const favBtn = document.querySelector(".fav-btn");
 
-    const title =
-        document.getElementById("title").value.trim();
+favBtn.addEventListener("click", () => {
 
-    const genre =
-        document.getElementById("genre").value.trim();
+    const quoteText = quote.innerText;
+    const authorText = author.innerText;
 
-    const rating = Number(
-        document.getElementById("rating").value
+    let favorites =
+        JSON.parse(localStorage.getItem("favorites")) || [];
+
+    favorites.push({
+        quote: quoteText,
+        author: authorText
+    });
+
+    localStorage.setItem(
+        "favorites",
+        JSON.stringify(favorites)
     );
 
-    if (
-        title === "" ||
-        genre === ""
-    ) {
-        alert("Please fill all fields");
-        return;
-    }
+    alert("add favorite !");
+});
 
-    if (
-        isNaN(rating) ||
-        rating < 1 ||
-        rating > 10
-    ) {
-        alert("Rating must be between 1 and 10");
-        return;
-    }
+quotebtn.innerHTML= '<i class="fa-solid fa-spinner fa-spin"></i>Loading Qutoe';
 
-    const movie = {
-        title,
-        genre,
-        rating
-    };
+const quoteAPIUrl ="https://dummyjson.com/quotes"
 
-    movies.push(movie);
+const getQuote = async (apiUrl )=>{
+    const result = await fetch(apiUrl);
+    const data = await result.json();
 
-    displayMovies();
+    const randomIndex = Math.floor(
+        Math.random() *data.quotes.length
+    );
+
+    const randomQuote = data.quotes[ randomIndex];
+
+    quote.innerText = randomQuote.quote;
+    author.innerText = randomQuote.author;
+
+     quoteCount++;
+     counter.innerText =
+        `Quotes Generated: ${quoteCount}`;
 
     
-    document.getElementById("title").value = "";
-    document.getElementById("genre").value = "";
-    document.getElementById("rating").value = "";
+    quotebtn.innerHTML ="Get new Quote";
+
+     console.log({
+     id: randomQuote.id,
+     quote: randomQuote.quote,
+     author: randomQuote.author
+});
 };
 
-
-const displayMovies = () => {
-
-    const movieList =
-        document.getElementById("movieList");
-
-    movieList.innerHTML = "";
-
-    movies.forEach((movie, index) => {
-
-        movieList.innerHTML += `
-            <div class="movie">
-                <h3>${index + 1}. ${movie.title}</h3>
-                <p>Genre: ${movie.genre}</p>
-                <p>Rating: ${movie.rating}/10</p>
-            </div>
-        `;
-    });
-};
-
-
-const startWatching = () => {
-
-    if (movies.length < 3) {
-        alert("Please add at least 3 movies");
-        return;
-    }
-
-    displayCurrentMovie();
-
-    document.getElementById("playBtn").disabled = false;
-    document.getElementById("pauseBtn").disabled = true;
-    document.getElementById("watchBtn").disabled = true;
-};
-
-const displayCurrentMovie = () => {
-
-    const currentMovie =
-        document.getElementById("currentMovie");
-
-    if (movies.length === 0) {
-
-        currentMovie.innerHTML = `
-            <h2>🎉 All Movies Watched!</h2>
-        `;
-
-        document.getElementById("playBtn").disabled = true;
-        document.getElementById("pauseBtn").disabled = true;
-        document.getElementById("watchBtn").disabled = true;
-
-        return;
-    }
-
-    const movie = movies[0];
-
-    currentMovie.innerHTML = `
-        <h2>${movie.title}</h2>
-        <p>Genre: ${movie.genre}</p>
-        <p>Rating: ${movie.rating}/10</p>
-    `;
-};
-
-
-const playMovie = () => {
-
-    document.getElementById("playBtn").disabled = true;
-
-    document.getElementById("pauseBtn").disabled = false;
-};
-
-
-const pauseMovie = () => {
-
-    document.getElementById("pauseBtn").disabled = true;
-
-    document.getElementById("watchBtn").disabled = false;
-};
-
-
-const watchMovie = () => {
-
-    if (movies.length === 0) {
-        return;
-    }
-
-    const watchedMovie = movies[0];
-
-    alert(`${watchedMovie.title} watched successfully 🎉`);
-
-    movies.shift();
-
-    displayMovies();
-
-    displayCurrentMovie();
-
-    if (movies.length > 0) {
-
-        document.getElementById("playBtn").disabled = false;
-        document.getElementById("pauseBtn").disabled = true;
-        document.getElementById("watchBtn").disabled = true;
-
-    } else {
-
-        document.getElementById("playBtn").disabled = true;
-        document.getElementById("pauseBtn").disabled = true;
-        document.getElementById("watchBtn").disabled = true;
-    }
-
-};
+getQuote(quoteAPIUrl);
